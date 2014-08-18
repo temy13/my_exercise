f190372d3015e89a419807b4255c5694b233260a
temy13 <temy.poporofier00@gmail.com>
2014-08-18 11:10:44 +0900

    タブ→空白4個　else if→elseif

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,14 +1,14 @@
 <?php
 
 for($i = 0; $i<100;$i++){
-	if($i % 3 == 0 && $i % 5 == 0)
-		echo "FizzBuzz\n";
-	else if($i % 3 == 0)
-		echo "Fizz\n";
-	else if($i % 5 == 0)
-		echo "Buzz\n";
-	else
-		echo $i."\n";
+    if($i % 3 == 0 && $i % 5 == 0)
+        echo "FizzBuzz\n";
+    elseif($i % 3 == 0)
+        echo "Fizz\n";
+    elseif($i % 5 == 0)
+        echo "Buzz\n";
+    else
+        echo $i."\n";
 
 }
 
--- sort.php
+++ sort.php
@@ -21,98 +21,96 @@ $marge_array = Marge($first_array, 0, $array_count-1);
 print_r($marge_array);
 
 function swap($array, $i, $j){
-	$temp = $array[$i];
-	$array[$i] = $array[$j];
-	$array[$j] = $temp;
-	return $array;
+    $temp = $array[$i];
+    $array[$i] = $array[$j];
+    $array[$j] = $temp;
+    return $array;
 }
 
 function TradeCheck($array, $i, $j){
-	if($array[$i] > $array[$j])
-		$array = swap($array,$i,$j);
+    if($array[$i] > $array[$j])
+        $array = swap($array,$i,$j);
 
-	return $array;
+    return $array;
 }
 //バブルソート
 function Bubble($array, $array_count){
-	for($i = 0; $i < $array_count; $i++){
-		for($j = $array_count-1; $j > $i; $j--){
-			$array = TradeCheck($array,$i,$j);
-		}
-	}
-	return $array;
+    for($i = 0; $i < $array_count; $i++){
+        for($j = $array_count-1; $j > $i; $j--){
+            $array = TradeCheck($array,$i,$j);
+        }
+    }
+    return $array;
 }
 //シェルソート
 function Shell($array, $array_count){
-	for($part = $array_count/2; (int)($part) > 0; $part /=2){
-			$part = (int)$part;
-			$array = InsertSort($array,$part,$array_count);
-	}
-	return $array;
+    for($part = $array_count/2; (int)($part) > 0; $part /=2){
+            $part = (int)$part;
+            $array = InsertSort($array,$part,$array_count);
+    }
+    return $array;
 }
 //単純挿入ソート
 function InsertSort($array, $part, $array_count){
-	for($i = 0; $i < $array_count-$part; $i++){
-		for($j = $i+$part; $j < $array_count; $j += $part){
-			$array = TradeCheck($array,$i,$j);
-		}	
-	}
-	return $array;
+    for($i = 0; $i < $array_count-$part; $i++){
+        for($j = $i+$part; $j < $array_count; $j += $part){
+            $array = TradeCheck($array,$i,$j);
+        }	
+    }
+    return $array;
 }
 //クイックソート　再帰的
 function Quick($array, $left, $right){
-	$l = $left;
-	$r = $right;
-	$pivot = $array[(int)(($left+$right)/2)];
-	while($l < $r){
-		//tradeする場所の探索
-		while($pivot > $array[$l])
-			$l++;
-		while($pivot < $array[$r])
-			$r--;
-		if($l >= $r) break;
-		$array = swap($array, $l, $r);
-		//次の探索へ
-		$l++; $r--;
-	}
-	if($l - $left > 1)
-		$array = quick($array, $left, $r);
-	if($right - $r > 1){
-		$array = quick($array, $l, $right);
-	}
-	return $array;
+    $l = $left;
+    $r = $right;
+    $pivot = $array[(int)(($left+$right)/2)];
+    while($l < $r){
+        //tradeする場所の探索
+        while($pivot > $array[$l])
+            $l++;
+        while($pivot < $array[$r])
+            $r--;
+        if($l >= $r) break;
+        $array = swap($array, $l, $r);
+        //次の探索へ
+        $l++; $r--;
+    }
+    if($l - $left > 1)
+        $array = quick($array, $left, $r);
+    if($right - $r > 1){
+        $array = quick($array, $l, $right);
+    }
+    return $array;
 }
 //マージソート
 function Marge($array, $left, $right){
-	//要素が一つだったら戻す
-	if($left >= $right){
-		$return_array = array($array[$left]);
-		return $return_array;
-	}
-	$center = (int)(($left+$right)/2);
-	$left_array = marge($array, $left, $center);
-	$right_array = marge($array, $center+1, $right);
-	$return_array = array();
-	$l = count($left_array); $r = count($right_array);
-	//結合した配列内でソート
-	for($n = $left; $n < $right; $n++){
-		if($left_array[0] < $right_array[0]){
-			$return_array[] = array_shift($left_array);
-			$l--;
-		}elseif($left_array[0] >= $right_array[0]){
-			$return_array[] = array_shift($right_array);
-			$r--;
-		}
-
-		if($l <= 0 ){
-			$return_array = array_merge($return_array, $right_array);
-			break;
-		}
-
-		if($r <= 0 ){
-			$return_array = array_merge($return_array, $left_array);
-			break;
-		}
-	}
-	return $return_array;
+    //要素が一つだったら戻す
+    if($left >= $right){
+        $return_array = array($array[$left]);
+        return $return_array;
+    }
+    $center = (int)(($left+$right)/2);
+    $left_array = marge($array, $left, $center);
+    $right_array = marge($array, $center+1, $right);
+    $return_array = array();
+    $l = count($left_array); $r = count($right_array);
+    //結合した配列内でソート
+    for($n = $left; $n < $right; $n++){
+        if($left_array[0] < $right_array[0]){
+            $return_array[] = array_shift($left_array);
+            $l--;
+        }elseif($left_array[0] >= $right_array[0]){
+            $return_array[] = array_shift($right_array);
+            $r--;
+        }
+        if($l <= 0 ){
+            $return_array = array_merge($return_array, $right_array);
+            break;
+        }
+        if($r <= 0 ){
+            $return_array = array_merge($return_array, $left_array);
+            break;
+        }
+    }
+    return $return_array;
 }
